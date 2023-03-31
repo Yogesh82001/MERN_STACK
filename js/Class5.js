48c4c9ce94b2281005302f559b931549f3b331bf
YogeshPanchal <yogeshpanchal28082001@gmail.com>
2023-03-31 17:01:19 +0530

    Assignement_1 work

--- js/Class5.js
+++ js/Class5.js
@@ -11,22 +11,22 @@ function add(){
             else{
                 sum = sum + (isNaN(parseInt(arguments[i][j]))?0:parseInt(arguments[i][j]) );
             }
-            }
+            
         } 
-        else if (typeof arguments[i]==='function'){
+     //   elseif (typeof arguments[i]==='function'){
         arguments[i] = arguments[i]();
             sum = sum + (isNaN(parseInt(arguments[i]))?0:parseInt(arguments[i]));
         }
-        else {
-            sum = sum + (isNaN(parseInt(arguments[i]))?0:parseInt(arguments[i]));
+        //else {
+          //  sum = sum + (isNaN(parseInt(arguments[i]))?0:parseInt(arguments[i]));
         }
         
     }
     return sum;
-}
+//}
 function one(){
     return 1;
 }
 function two(){
     return 2;
-}
+}//}
